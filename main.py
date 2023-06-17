03fda86985cede316615abc415cd10d463be20d4
Mirac Can Yilmaz <miraccan6161@gmail.com>
2023-06-17 03:59:11 +0300

    ArgoCD installation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import subprocess
 from djangostart.continues_integration import continues_integration
 from updatehelmchart import update_helm_chart_image_tag
 from gitcommit import commit
+import time
 
 def install_project():
     subprocess.run(['sh', 'start.sh'])
@@ -12,6 +13,7 @@ def main():
         option = input("Select option: ")
         if option == "1":
             continues_integration("microservicename", "1.0.0")
+            time.sleep(20)
             values_file_path = "deploymentchart/values.yaml"
             new_image_tag = "1.0.0"
             update_helm_chart_image_tag(values_file_path, new_image_tag)
