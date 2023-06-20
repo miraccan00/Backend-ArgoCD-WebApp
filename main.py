4d984ea0a4459869ab0669905414eb29f252b265
Mirac Can Yilmaz <miraccan6161@gmail.com>
2023-06-20 20:29:05 +0300

    ArgoCD installation

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
         print("1. Continues integration")
         option = input("Select option: ")
         if option == "1":
-            version = "1.0.3"
+            version = "1.0.4"
             if continues_integration("microservicename", version):
                 values_file_path = "deploymentchart/values.yaml"
                 new_image_tag = version
